8454035b0a3994e8b0c556e60e3dfb7d1c264e5e
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-11-08 23:44:22 -0500

    folders

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+
+function Contact(props) {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <h3>Github</h3>
+      <h3>LinkedIn</h3>
+      <h3>Email</h3>
+    </div>
+  )
+}
+
+export default Contact;
