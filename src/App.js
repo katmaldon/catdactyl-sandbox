1870f2fc975746af271a6a18238603a22891c886
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-11-08 23:45:35 -0500

    components in app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
+import Navbar from './Components/Navbar';
+import HomeContainer from './Containers/HomeContainer';
+import AboutContainer from './Containers/AboutContainer';
+import GalleryContainer from './Containers/GalleryContainer';
+import ContactContainer from './Containers/ContactContainer';
 
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className='App'>
+                <Navbar />
+                <Switch>
+                    <Route exact path='/' render={(routerProps) => <HomeContainer {...routerProps} />} />
+                    <Route path='/contact' render={() => <ContactContainer />} />
+                    <Route path='/about' render={() => <AboutContainer />} />
+                    <Route path='/projects' render={() => <GalleryContainer />} />
+                </Switch>
+                <footer>© {new Date().toISOString().substring(0, 4)} Anca Risca</footer>
+            </div>
+        );
+    }
 }
 
 export default App;
