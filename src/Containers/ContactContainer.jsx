49a41bb2f758029aa09d1761485b5dfd2ac59efe
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-11-08 23:47:53 -0500

    home container props

---
+++ src/Containers/ContactContainer.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Contact from '../Components/Contact';
+
+class ContactContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="contact">
+                    <Contact />
+                </div>
+            </div>
+        );
+    };
+};
+
+export default ContactContainer;
