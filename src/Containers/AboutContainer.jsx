7839036bc9a112637d0b24ca66dda95849d0ec35
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-11-08 23:47:01 -0500

    containers

---
+++ src/Containers/AboutContainer.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import About from '../Components/About';
+
+class AboutContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="about">
+                    <About />
+                </div>
+            </div>
+        );
+    };
+};
+
+export default AboutContainer;
